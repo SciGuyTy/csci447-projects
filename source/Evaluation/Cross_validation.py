b9ebcfd6a2c366249e44a162b7f7a28634feea10
SciGuyTy <10765359+SciGuyTy@users.noreply.github.com>
2022-09-02 14:44:00 -0600

    ML-6: Added stratification method

--- source/Evaluation/Cross_validation.py
+++ source/Evaluation/Cross_validation.py
@@ -1,17 +1,17 @@
-from typing import Callable, Union
+from typing import Callable, Union, Any
 import pandas as pd
 import numpy as np
 
 Column_label = Union[str, int]
 
 
-class Cross_validation:
-    data: pd.DataFrame = None
-    classification_column: pd.Series = None
-
-    def set_data(
-        self, data: pd.DataFrame, classification_label: Column_label = "class"
-    ) -> None:
+class CrossValidation:
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        classification_label: Column_label = "class",
+        positive_class_value: Any = True,
+    ):
         """Set the dataset to be used for cross-validating a model
 
         Parameters
@@ -21,19 +21,63 @@ class Cross_validation:
 
         (Optional) classification_label: Column_label
             The label (or index) representing the column storing classification data (defaults to "class").
+
+        (Optional) positive_class_value: Any
+            The value representing a positive outcome for the classification column (defaults to True).
         """
 
         # Store the data to be used for training and evaluating the model(s)
-        self.data = data
+        self.data: pd.DataFrame = data
 
-        # Store the column which stores classification data
-        self.classification_column = data[classification_label]
+        # Store the classification column name
+        self.classification_column_name: Column_label = classification_label
+
+        # Store the classification positive value
+        self.positive_class_value = positive_class_value
+
+    def __fold_data(self, num_folds: int, stratify: bool):
+        """Divide a dataset into folds (for cross-validation)
+        
+        Parameters
+        ----------
+        num_folds: int
+            The number of folds (number of 'chunks' with which to split the data).
+
+        stratify: bool
+            Whether the given folds should be stratified (i.e., split in a manner such that 
+            the proportion of classifications within the dataset is similarly represented in each fold).
+
+        """
+        # Define the levels of classification in the data
+        classification_levels = self.data[self.classification_column_name].unique()
+
+        # Shuffle the data into a random order
+        shuffled_data = self.data.sample(frac=1)
+
+        # An array to hold the data while it is being folded
+        folded_data = []
+        
+        if(stratify):
+            # If the data is to be stratified, iterate through each classification level and divide the data into equally sized chunks based on the number of folds
+            for classification in classification_levels:
+                class_data = shuffled_data[shuffled_data[self.classification_column_name] == classification]
+
+                # Divide the data for a given class into equally sized chunks
+                folded_data.append(np.array_split(class_data, num_folds))
+
+            # Stack all of the respective 'columns' (equally divided datasets for each class) and combine them into a fold
+            # Then return the list of folded data
+            return list(map(lambda x: pd.concat(x), np.column_stack((folded_data))))
+
+        else:
+            # If the data is not to be stratified, simply return a list of equally sized chunks of data from the dataset
+            return np.array_split(shuffled_data, num_folds)
 
     def validate(
-        self,
+        self, 
         model: Callable,
-        loss_function: Callable,
         num_folds: int = 10,
+        stratify: bool = False
     ) -> float:
         """Perform cross-validation using k=num_folds folds
 
@@ -42,26 +86,21 @@ class Cross_validation:
         model: Callable
             The model to perform cross-validation on.
 
-        loss_function: Callable
-            The loss function to use.
-
         (Optional) num_folds: int
             The number of times to sample from the dataset (defaults to 10).
         """
 
-        # Shuffle the data
-        self.data = self.data.sample(frac=1)
-
         # Divide the data into k folds
-        folded_data = np.array_split(self.data, num_folds)
+        folded_data = self.__fold_data(num_folds, stratify)
 
-        # Total loss value (used to calculate average loss value)
-        total_loss = 0
+        # Results for all the folds
+        overall_results = []
 
         # Iterate through each fold and run the model
         for index, fold in enumerate(folded_data):
-            # Array to store prediction results for this fold
-            prediction_results = pd.DataFrame(columns=["actual", "prediction"])
+
+            # Results for this fold
+            fold_results = {"TP": 0, "TN": 0, "FP": 0, "FN": 0}
 
             # Define the data for testing (a single fold)
             test_data = fold
@@ -73,15 +112,26 @@ class Cross_validation:
             # Combine training data into a single DataFrame
             training_data = pd.concat(training_data)
 
+            algorithm = model(training_data, self.classification_column_name)
+            algorithm.train()
+
             # Perform prediction on all samples for this test fold
             for _, sample in test_data.iterrows():
                 # Train and execute the model on the given training data and testing data
-                prediction_results.loc[len(prediction_results)] = model(
-                    training_data, sample
-                )
+                prediction = algorithm.predict(sample)
 
-            # Implement the loss function and update the total loss value
-            total_loss += loss_function(prediction_results)
+                # Determine whether the prediction is a true positive, false positive, true negative, or false negative
+                if prediction == self.positive_class_value:
+                    if prediction == sample[self.classification_column_name]:
+                        fold_results["TP"] += 1
+                    else:
+                        fold_results["FP"] += 1
+                else:
+                    if prediction == sample[self.classification_column_name]:
+                        fold_results["TN"] += 1
+                    else:
+                        fold_results["FN"] += 1
+            overall_results.append(fold_results)
 
         # Return the average loss value
-        return total_loss / num_folds
+        return overall_results
