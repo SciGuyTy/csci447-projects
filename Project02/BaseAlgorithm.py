afa600f26398a0dc1a321590a6f42b2c137914bc
Braeden Hunt <bjhunt918@gmail.com>
2022-10-01 12:21:09 -0600

    Fixed lots of issues with KNN

--- Project02/BaseAlgorithm.py
+++ Project02/BaseAlgorithm.py
@@ -2,49 +2,58 @@ from abc import ABC, abstractmethod
 import pandas as pd
 import math
 from heapq import heappop, heappush, heapify
+from itertools import count
+
 
 class BaseAlgorithm(ABC):
 
-    def __init__(self, attributes, class_col: str, training_data, k, h, sigma, regression=False):
+    def __init__(self, attributes, class_col: str, training_data: pd.DataFrame, regression=False):
         self.attributes = attributes
         self.class_col = class_col
-        self.training_data = training_data
+        self.training_data: pd.DataFrame = training_data
         self.regression = regression
         self.classes = self.training_data[self.class_col].unique()
-
-        self.h = h
-        self.sigma = sigma
-        self.k = k
+        self.tiebreaker = count()
 
     def predict(self, instance, k):
-        pass
+        neighbors_distance = self.find_k_nearest_neighbors(instance, k)
+        if self.regression:
+            pass
+        else:
+            neighbors = [i[1] for i in neighbors_distance]
+            return self.vote(neighbors)
 
     def find_k_nearest_neighbors(self, instance, k):
         heap = []
         heapify(heap)
-        for neighbor in self.training_data:
-            distance = -1 * self.minkowski_metric(neighbor, instance) # Multiple by negative one to use Max Heap
+        for i in range(len(self.training_data.index)):
+            neighbor = self.training_data.iloc[i]
+            distance = -1 * self.minkowski_metric(neighbor, instance)  # Multiply by negative one to use Max Heap
             if len(heap) < k:
-                heappush(heap, (distance, neighbor))
-            elif abs(distance) < abs(heap[0]):
+                heappush(heap, (distance, next(self.tiebreaker), neighbor))
+            elif abs(distance) < abs(heap[0][0]):
                 heappop(heap)
-                heappush(heap, (distance, neighbor))
-        return heap # Flip distance TODO
+                heappush(heap, (distance, next(self.tiebreaker), neighbor))
+        return [(-i[0], i[2]) for i in heap]  # Flip distance
 
     def regress(self, neighbors, instance):
         total = 0
         for t in neighbors:
             pass
             # TODO
-        
+
     def vote(self, neighbors):
-        return neighbors[self.class_col].mode()
+        df = pd.DataFrame(data=neighbors)
+        mode = df[self.class_col].mode()
+        if len(mode) > 1:
+            mode = mode.sample()
+        return mode.iloc[0]
 
     def gaussian_kernel(self, u):
-        return math.e**((u**2)/-2) / math.sqrt(2*math.pi)
+        return math.e ** ((u ** 2) / -2) / math.sqrt(2 * math.pi)
 
     def minkowski_metric(self, x, y, p=2):
         dist = 0
         for attr_label in self.attributes:
-            dist += abs(x[attr_label] - y[attr_label])**p
+            dist += abs(float(x[attr_label]) - float(y[attr_label])) ** p
         return dist
--- Project02/main.py
+++ Project02/main.py
@@ -1,7 +1,33 @@
-
+from BaseAlgorithm import BaseAlgorithm
+import pandas as pd
 
 def main():
     pass
 
+def test_knn_on_breast_cancer():
+    filepath = "../datasets/classification/BreastCancer/breast-cancer-wisconsin.data"
+    column_headers = [
+        "id",
+        "clump",
+        "size",
+        "shape",
+        "adhesion",
+        "epithelial_size",
+        "nuclei",
+        "chromatin",
+        "nucleoli",
+        "mitoses",
+        "class",
+    ]
+
+    data = pd.read_csv(filepath, names=column_headers, converters={"class": lambda x: (int(x) == 4)})
+    rows_to_drop = [1057013, 1057013, 1096800, 1183246, 1184840, 1193683, 1197510, 1241232, 169356, 432809,
+                    563649, 606140, 61634, 704168, 733639, 1238464, 1057067]
+
+    data = data[~data['id'].isin(rows_to_drop)]
+    knn = BaseAlgorithm(column_headers[1:-1], "class", data)
+
+    print(knn.predict(data.iloc[0], 5))
+
 if __name__ == "__main__":
-    main()
+    test_knn_on_breast_cancer()
