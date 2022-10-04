44237ea58b1029bc40148b2e8801c640179af012
SciGuyTy <10765359+SciGuyTy@users.noreply.github.com>
2022-10-03 21:15:28 -0600

    ML-33: Initial implementation of CV w/ support for regression

---
+++ Project02/source/Evaluation/CrossValidation.py
@@ -0,0 +1,176 @@
+from typing import Callable, List, Union, Any
+import pandas as pd
+import numpy as np
+
+Column_label = Union[str, int]
+
+
+class CrossValidation:
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        target_feature: Column_label = "class",
+    ):
+        """Set the dataset to be used for cross-validating a model
+
+        Parameters
+        ----------
+        data: pd.DataFrame
+            The dataset to be used for cross-validating a model.
+
+        (Optional) classification_label: Column_label
+            The label (or index) representing the column storing classification data (defaults to "class").
+
+        (Optional) positive_class_value: Any
+            The value representing a positive outcome for the classification column (defaults to True).
+
+        (Optional) target_feature: Column_label
+            The label of the feature which is being used as the target (defaults to "class"),
+        """
+
+        # Store the data to be used for training and evaluating the model(s)
+        self.data: pd.DataFrame = data
+
+        # Store the classification column name
+        self.target_feature = target_feature
+
+        # The algorithm
+        self.algorithm = None
+
+    def __fold_data(self, num_folds: int, stratify: bool, regression: bool = False):
+        """Divide a dataset into folds (for cross-validation)
+
+        Parameters
+        ----------
+        num_folds: int
+            The number of folds (number of 'chunks' with which to split the data).
+
+        stratify: bool
+            Whether the given folds should be stratified (i.e., split in a manner such that
+            the proportion of classifications within the dataset is similarly represented in each fold).
+
+        Returns
+        -------
+        folded_data: List[pd.DataFrame]
+            A list of k=num_folds DataFrames containing a fold of data
+
+        """
+        # Shuffle the data into a random order
+        shuffled_data = self.data.sample(frac=1)
+
+        # A list to hold the folded data
+        folded_data = [pd.DataFrame(columns=self.data.columns)] * num_folds
+
+        if stratify:
+            if regression:
+                # Sort the data based on the target feature
+                sorted_data = shuffled_data.sort_values(self.target_feature)
+
+                fold_size = int(len(sorted_data.index) / num_folds)
+
+                # Decompose data into 10 'bins' of consecutive data
+                split_data = np.array_split(sorted_data, fold_size)
+
+                for fold_id in range(len(folded_data)):
+                    for group in split_data:
+                        # For each fold, select the corresponding value from each group in the
+                        # split value array (so for example, the first fold will grab the first
+                        # element from each group in the split value array)
+                        folded_data[fold_id] = pd.concat(
+                            [folded_data[fold_id], group.iloc[fold_id].to_frame().T],
+                            ignore_index=True,
+                        )
+
+            else:
+                # Define the levels of classification in the data
+                classification_levels = self.data[self.target_feature].unique()
+
+                # If the data is to be stratified, iterate through each classification level
+                # and divide the data into equally sized chunks based on the number of folds
+                for classification in classification_levels:
+                    class_data = shuffled_data[
+                        shuffled_data[self.target_feature] == classification
+                    ]
+
+                    # Divide the data for a given class into equally sized chunks
+                    split_data = np.array_split(class_data, num_folds)
+
+                    # "Stack" each kth chunk of data with its counterparts from the other classes
+                    for index, data in enumerate(split_data):
+                        folded_data[index] = pd.concat([data, folded_data[index]])
+
+            # Return the folded data
+            return folded_data
+
+        else:
+            # If the data is not to be stratified, simply return a list of equally sized
+            # chunks of data from the dataset
+            return np.array_split(shuffled_data, num_folds)
+
+    def validate(
+        self,
+        model: Callable,
+        num_folds: int = 10,
+        stratify: bool = False,
+        regression: bool = False,
+        model_params=[],
+        predict_params=[],
+    ) -> float:
+        """Perform cross-validation using k=num_folds folds
+
+        Parameters
+        ----------
+        model: Callable
+            The model to perform cross-validation on.
+
+        (Optional) num_folds: int
+            The number of times to sample from the dataset (defaults to 10).
+
+        (Optional) alter_data: bool
+            Whether to alter the training data (defaults to False).
+        """
+
+        # Divide the data into k folds
+        folded_data = self.__fold_data(num_folds, stratify, regression)
+
+        # Results for all the folds
+        overall_results = []
+
+        # Iterate through each fold and run the model
+        for index, fold in enumerate(folded_data):
+            # Define the data for testing (a single fold)
+            test_data = fold
+
+            # Define the data for training (remaining folds)
+            training_data = folded_data.copy()
+            training_data.pop(index)
+
+            # Combine training data into a single DataFrame
+            training_data = pd.concat(training_data)
+
+            self.algorithm = model(training_data, self.target_feature, *model_params)
+
+            # Get a list of all class levels
+            classes = self.data[self.target_feature].unique()
+
+            # Results for this fold
+            fold_results = pd.DataFrame(columns=["actual", "predicted"])
+
+            if not regression:
+                fold_results = pd.DataFrame(0, columns=classes, index=classes)
+
+            # Perform prediction on all samples for this test fold
+            for _, sample in test_data.iterrows():
+                # Train and execute the model on the given training data and testing data
+                prediction = self.algorithm.predict(sample, *predict_params)
+
+                if regression:
+                    fold_results.loc[0] = [sample[self.target_feature], prediction]
+                else:
+                    # Increment the prediction/actual pair in the confusion matrix
+                    fold_results[sample[self.target_feature]][prediction] += 1
+
+            overall_results.append(fold_results)
+
+        # Return the average loss value
+        return overall_results
---
+++ Project02/source/Evaluation/EvaluationMeasure.py
@@ -0,0 +1,90 @@
+import re
+import pandas as pd
+
+
+class EvaluationMeasure:
+    @classmethod
+    def calculate_precision(cls, results: pd.DataFrame, positive_class: str):
+        """Calculates the precision based on the results
+
+        Parameters
+        ----------
+        results: pd.DataFrame
+            The results from the experiment
+
+        positive_class: str
+            The positive class in the calculation
+        """
+
+        num_true_positives = results[positive_class][positive_class]
+        num_predicted_positives = results.sum(axis=1)[positive_class]
+
+        # All predictions are negative, and 0 positive predictions were reported so the precision approaches 1
+        if num_predicted_positives == 0:
+            return 1
+
+        return num_true_positives / num_predicted_positives
+
+
+    @classmethod
+    def calculate_recall(cls, results: pd.DataFrame, positive_class: str):
+        """Calculates the recall based on the results
+
+        Parameters
+        ----------
+        results: pd.DataFrame
+            The results from the experiment
+
+        positive_class: str
+            The positive class in the calculation
+        """
+
+        num_true_positives = results[positive_class][positive_class]
+        num_predicted_positives = results.sum(axis=0)[positive_class]
+
+        # No positive cases in the input data, and 0 positive classes were predicted so the recall is 1?
+        if num_predicted_positives == 0:
+            return 1
+
+        return num_true_positives / num_predicted_positives
+
+    @classmethod
+    def calculate_f_beta_score(cls, results: pd.DataFrame, positive_class: str, beta: int = 1):
+        """Calculates the F_Beta score based on the results and the beta value
+
+        Parameters
+        ----------
+        results: pd.DataFrame
+            The results from the experiment
+
+        (Optional) beta: int
+            The beta value to use when calculating the F_Beta score. Default is 1
+
+        positive_class: str
+            The positive class in the calculation
+        """
+        precision = cls.calculate_precision(results, positive_class)
+        recall = cls.calculate_recall(results, positive_class)
+        denom = precision * beta**2 + recall
+        
+        if denom == 0:
+            return 0.0
+
+        return ((1 + beta**2) * precision * recall) / denom
+
+    @classmethod
+    def calculate_0_1_loss(cls, results: pd.DataFrame):
+        """Calculates the 0-1 loss measure based on the results
+
+        Parameters
+        ----------
+        results: dict[str, int]
+            The results from the experiment
+        """
+        num_correct_predictions = 0
+
+        # Compute the total number of correct predictions (TP for each class level)
+        for column in results.columns:
+            num_correct_predictions += results[column][column]
+
+        return num_correct_predictions / results.to_numpy().sum()
