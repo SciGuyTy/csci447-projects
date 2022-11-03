Fixing delta computation

BP/Layer.py
@@ -20,10 +20,11 @@ class Layer:
         self.weights = self._generate_random_weights()
         self.output = None
 
+        self.error = None
         self.delta = None
         
-        # if(prev_layer):
-        #     self.weight_change = [np.zeros((prev_layer.num_nodes, num_nodes))]
+        if(prev_layer):
+            self.weight_change = [np.zeros((prev_layer.num_nodes, num_nodes))]
 
         pass
 
@@ -37,7 +38,7 @@ class Layer:
 
     def compute_output(self, input):
         # Compute action potential
-        linear_combination = np.matmul(input, self.weights)
+        linear_combination = np.matmul(self.weights, input)
         
         if(self.bias):
             linear_combination += self.bias

BP/NeuralNetwork.py
@@ -14,17 +14,37 @@ class NeuralNetwork():
             prev_output = layer.output
 
     def backpropagate(self, target, learning_rate, momentum):
-        self.layers[-1].delta = 2 * np.subtract(self.layers[-1].output, target)
+        self.layers[-1].error = 1/2 * np.subtract(target, self.layers[-1].output)**2
 
-        for index in range(len(self.layers) - 2, 0, -1):
+        # Output delta
+        # print(-(target - self.layers[-1].output))
+        # print(self.layers[-1].output * (1 - self.layers[-1].output))
+        # print(self.layers[-2].output)
+
+        for index in range(len(self.layers) - 1, 0, -1):
             # Get reference to current, preceding, and following layer
             curr_layer = self.layers[index]
-            prev_layer = self.layers[index + 1]
+            next_layer = self.layers[index - 1]
+            if(index + 1 > len(self.layers) - 1):
+                prev_layer = None
+            else:
+                prev_layer = self.layers[index + 1]
 
-            curr_layer.delta = np.matmul(prev_layer.delta, prev_layer.weights.T)
+            # Compute delta for output layer
+            if (prev_layer == None):
+                # print(-(target - self.layers[-1].output) * self.layers[-1].output * (1 - self.layers[-1].output))
+                curr_layer.delta = (-(target - self.layers[-1].output) * self.layers[-1].output * (1 - self.layers[-1].output))[:, np.newaxis] * np.tile(next_layer.output, (2, 1))
+            else:
+                print(-(target - self.layers[-1].output))
+                # print(-(target - self.layers[-1].output) * self.layers[-1].output * (1 - self.layers[-1].output))
+                # print(-(target - self.layers[-1].output) * self.layers[-1].output * (1 - self.layers[-1].output)[:, np.newaxis] * prev_layer.weights)
 
-            curr_layer.weights -= learning_rate * curr_layer.delta
-            # curr_layer.weight_change.append((learning_rate * curr_layer.delta * curr_layer.output))
+            curr_layer.weight_change.append(learning_rate * curr_layer.delta)
+
+        #     curr_layer.delta = np.matmul(prev_layer.delta, prev_layer.weights.T)
+
+        #     curr_layer.weights -= learning_rate * curr_layer.delta
+        #     # curr_layer.weight_change.append((learning_rate * curr_layer.delta * curr_layer.output))
 
     def train(self, training_data, target_feature, epochs, batch_size, learning_rate, momentum):
         for i in range(epochs):
@@ -66,19 +86,21 @@ if __name__ == "__main__":
     PP.load_raw_data_from_file(file_path, ["a", "b", "class"])
 
 
-    layer1 = Layer(2)
-    layer2 = Layer(5, layer1, activation=np.vectorize(sigmoid))
-    layer3 = Layer(5, layer2, activation=np.vectorize(sigmoid))
-    layer4 = Layer(2, layer3, activation=softmax)
 
-    layers = [layer1, layer2, layer3, layer4]
+    for i in range(1):
+        layer1 = Layer(2)
+        layer2 = Layer(2, layer1, bias=[0.35], activation=np.vectorize(sigmoid))
+        layer3 = Layer(2, layer2, bias=[0.60], activation=np.vectorize(sigmoid))
 
-    network = NeuralNetwork(layers)
-    network.train(PP.data, "class", 50, 1, 0.1, 0.2)
+        layer2.weights = np.array([[0.15, 0.2], [0.25, 0.3]])
+        layer3.weights = np.array([[0.40, 0.45], [0.5, 0.55]])
 
-    network.feed_forward([1, 0])
-    res = layer4.output
+        layers = [layer1, layer2, layer3]
 
-    print(layer3.output)
-    print(layer4.output)
+        network = NeuralNetwork(layers)
+        # network.train(PP.data, "class", 50, 1, 0.1, 0.2)
 
+        network.feed_forward([0.05, 0.1])
+        print(layer3.output)
+        network.backpropagate([0.01, 0.99], 0.1, 0.1)
+        print(layer3.error)